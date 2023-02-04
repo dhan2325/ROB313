4835249c72826e5c86010f82ec951dd14a04f1e1
han.daniel <danihan2325@gmail.com>
2023-02-04 18:13:49 -0500

    created randomizer function

--- labs/lab1/lab1_q1.py
+++ labs/lab1/lab1_q1.py
@@ -11,11 +11,11 @@ from data.data_utils import load_dataset # include copy of data folder in submis
 import numpy as np
 from math import sqrt
 '''
-x train, x valid, x test, y train, y valid, y test = load dataset(’mauna loa’)
-x train, x valid, x test, y train, y valid, y test = load dataset(’rosenbrock’, n train=5000, d=2)
-x train, x valid, x test, y train, y valid, y test = load dataset(’pumadyn32nm’)
-x train, x valid, x test, y train, y valid, y test = load dataset(’iris’)
-x train, x valid, x test, y train, y valid, y test = load dataset(’mnist small’)
+x train, x valid, x test, y train, y valid, y test = load dataset('mauna loa')
+x train, x valid, x test, y train, y valid, y test = load dataset('rosenbrock', n train=5000, d=2)
+x train, x valid, x test, y train, y valid, y test = load dataset('pumadyn32nm')
+x train, x valid, x test, y train, y valid, y test = load dataset('iris')
+x train, x valid, x test, y train, y valid, y test = load dataset('mnist small')
 '''
 
 # l1 is manhattan dist, l2 is euclidian dist
@@ -33,7 +33,17 @@ def l2(vec1 : np.ndarray, vec2 : np.ndarray):
     for i in range(vec1.shape[0]):
         distance += (vec1[i] - vec2[i])**2
     return sqrt(distance)
+
+
+def randomize(array_x : np.ndarray, array_y: np.ndarray, split_axis : int = 1):
+    assert array_x.shape == array_y.shape
+    joined = np.concatenate((array_x, array_y), axis=split_axis)
     
+    np.random.shuffle(joined)
+    [shuf_x, shuf_y] = np.split(joined, 2, axis = 1)
+    for i in range(3):
+        print(shuf_x[i], shuf_y[i])
+
 
 
 # to keep variable naming and code readable, perform regression for each dataset in individual functions
@@ -43,13 +53,22 @@ def knn_mauna():
     x_train, y_train = np.vstack([x_train, x_valid]), np.vstack([y_train, y_valid])
     
     assert x_train.shape == y_train.shape, "training data shape invalid"
+    
+
     # want even multiples of five, discarding an insignificant amount of data
     while(x_train.shape[0] %5):
         x_train = x_train[:-1]
     while(y_train.shape[0] %5):
         y_train = y_train[:-1]
+    for i  in range (3):
+        print(x_train[i], y_train[i])
 
     [xt1, xt2, xt3, xt4, xt5] = np.split(x_train, 5, axis = 0)
+    randomize(x_train, y_train, split_axis = 1)
+
+    # five separate sets of data, 
+
+
 
 
 
